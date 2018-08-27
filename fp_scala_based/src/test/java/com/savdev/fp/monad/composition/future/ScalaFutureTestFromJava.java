dd97b880baf50b8a84719f0eaada3a87e30273b3
alexandr <alexandrsokolov@users.noreply.github.com>
2018-08-27 12:31:20 +0200

    corrected java test to use default implicit future

--- fp_scala_based/src/test/java/com/savdev/fp/monad/composition/future/ScalaFutureTestFromJava.java
+++ fp_scala_based/src/test/java/com/savdev/fp/monad/composition/future/ScalaFutureTestFromJava.java
@@ -2,8 +2,10 @@ package com.savdev.fp.monad.composition.future;
 
 import com.savdev.fp.monad.composition.future.scala.FailedCappuchino;
 import com.savdev.fp.monad.composition.future.scala.TestCappuchino;
+import com.savdev.fp.monad.composition.future.scala.TestUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import scala.concurrent.ExecutionContext;
 import scala.concurrent.ExecutionContext$;
 import scala.concurrent.Future;
 import scala.util.Try;
@@ -11,10 +13,22 @@ import scala.util.Try;
 import java.util.concurrent.TimeUnit;
 
 public class ScalaFutureTestFromJava {
+  ExecutionContext scalaGlobalEc = ExecutionContext$.MODULE$.global();
   @Test
   public void testSequential() throws InterruptedException {
     Future<String> result = TestCappuchino.prepareCappuccinoSequentially();
-    result.onComplete(onCompleteHandler, ExecutionContext$.MODULE$.global());
+    //result.onComplete(onCompleteHandler, );
+
+    result.onComplete(TestUtils.onCompleteHandler(), scalaGlobalEc);
+    while (!result.isCompleted()) {
+      TimeUnit.SECONDS.sleep(2);
+    }
+  }
+
+  @Test
+  public void testSequentialViaOnCompleteJavaHandler() throws InterruptedException {
+    Future<String> result = TestCappuchino.prepareCappuccinoSequentially();
+    result.onComplete(onCompleteJavaHandler, scalaGlobalEc);
     while (!result.isCompleted()) {
       TimeUnit.SECONDS.sleep(2);
     }
@@ -23,7 +37,7 @@ public class ScalaFutureTestFromJava {
   @Test
   public void testAsynchroniously() throws InterruptedException {
     Future<String> result = TestCappuchino.prepareCappuccinoAsynchroniously();
-    result.onComplete(onCompleteHandler, ExecutionContext$.MODULE$.global());
+    result.onComplete(TestUtils.onCompleteHandler(), scalaGlobalEc);
     while (!result.isCompleted()) {
       TimeUnit.SECONDS.sleep(2);
     }
@@ -32,22 +46,13 @@ public class ScalaFutureTestFromJava {
   @Test
   public void testFailedCappuchino() throws InterruptedException {
     Future<String> result = FailedCappuchino.prepareCappuccinoAsynchroniously();
-    result.onComplete(r-> {
-      if (r.isFailure()){
-        Assert.assertEquals("Cannot froth", r.failed().get().getMessage());
-      } else if (r.isSuccess()){
-        Assert.fail("Cannot be successul");
-      } else {
-        Assert.fail("Not expected status");
-      }
-      return null;
-    }, ExecutionContext$.MODULE$.global());
+    result.onComplete(TestUtils.onCompleteErrorHandler(), scalaGlobalEc);
     while (!result.isCompleted()) {
       TimeUnit.SECONDS.sleep(2);
     }
   }
 
-  private scala.Function1<Try<String>, String> onCompleteHandler = r -> {
+  private scala.Function1<Try<String>, String> onCompleteJavaHandler = r -> {
     if (r.isSuccess()){
       System.out.println("onComplete successfully, thread: "
         + Thread.currentThread().getName());
@@ -60,56 +65,4 @@ public class ScalaFutureTestFromJava {
     }
     return null;
   };
-//
-//  ExecutionContext ec = ExecutionContext$.MODULE$.global();
-//
-//  @Test
-//  public void testSequential() throws InterruptedException {
-//    Future<String> result = TestCappuchino.prepareCappuccinoSequentially(ec);
-//    result.onComplete(onCompleteHandler, ec);
-//    while (!result.isCompleted()) {
-//      TimeUnit.SECONDS.sleep(2);
-//    }
-//  }
-//
-//  @Test
-//  public void testAsynchroniously() throws InterruptedException {
-//    Future<String> result = TestCappuchino.prepareCappuccinoAsynchroniously(ec);
-//    result.onComplete(onCompleteHandler, ec);
-//    while (!result.isCompleted()) {
-//      TimeUnit.SECONDS.sleep(2);
-//    }
-//  }
-//
-//  @Test
-//  public void testFailedCappuchino() throws InterruptedException {
-//    Future<String> result = FailedCappuchino.prepareCappuccinoAsynchroniously(ec);
-//    result.onComplete(r-> {
-//      if (r.isFailure()){
-//        Assert.assertEquals("Cannot froth", r.failed().get().getMessage());
-//      } else if (r.isSuccess()){
-//        Assert.fail("Cannot be successul");
-//      } else {
-//        Assert.fail("Not expected status");
-//      }
-//      return null;
-//    }, ec);
-//    while (!result.isCompleted()) {
-//      TimeUnit.SECONDS.sleep(2);
-//    }
-//  }
-//
-//  private scala.Function1<Try<String>, String> onCompleteHandler = r -> {
-//    if (r.isSuccess()){
-//      System.out.println("onComplete successfully, thread: "
-//        + Thread.currentThread().getName());
-//      Assert.assertEquals("cappuccino", r.get());
-//      return r.get();
-//    } else if (r.isFailure()){
-//      Assert.fail("Cannot be failed");
-//    } else {
-//      Assert.fail("Not expected status");
-//    }
-//    return null;
-//  };
 }
